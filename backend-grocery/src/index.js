fe5b99a387de7da5d5f7113aa0edd3dd4acd66dc
TuanPA2002 <phianhtuan31@gmail.com>
2023-06-14 16:31:51 +0700

    Create database and Connect to mongooDB

--- backend-grocery/src/index.js
+++ backend-grocery/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const mongoose = require("mongoose")
 dotenv.config()
 
 const app = express()
@@ -7,6 +8,14 @@ const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
     return res.send('Hello world...')
+})
+
+mongoose.connect(`mongodb+srv://Tuan:${process.env.MONGOO_DB}@cluster0.agpirc5.mongodb.net/?retryWrites=true&w=majority`)
+.then(() => {
+    console.log("Connect mongoose database success!")
+})
+.catch((err) => {
+    console.log(err)
 })
 
 app.listen(port, () => {
