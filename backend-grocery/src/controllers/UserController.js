58f8b2e40dc3c58d6d40d4d986902d2d0e2efd59
TuanPA2002 <phianhtuan31@gmail.com>
2023-06-19 21:50:03 +0700

    #18 - Create API User

--- backend-grocery/src/controllers/UserController.js
+++ backend-grocery/src/controllers/UserController.js
@@ -2,10 +2,30 @@ const UserService = require('../services/UserService')
 
 const createUser = async (req, res) => {
     try {
-        console.log(req.body);
-        const res = await UserService.createUser()
+        const {name, email, password, confirmPassword, phone} = req.body
+        const regex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        const ischeckEmail = regex.test(email)
 
-        return res.status(200).json(res)
+        if (!name || !email || !password || !confirmPassword || !phone) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The imput is required'
+            })
+        } else if (!ischeckEmail) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'Wrong email format'
+            })
+        } else if (password !== confirmPassword) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The password is equal confirm password'
+            })
+        }
+
+        const respononse = await UserService.createUser(req.body)
+
+        return res.status(200).json(respononse)
     } catch (e) {
         return res.status(404).json({
             message: e
--- backend-grocery/src/services/UserService.js
+++ backend-grocery/src/services/UserService.js
@@ -1,13 +1,40 @@
-const createUser = () => {
-    return new Promise((resolve, reject) => {
+const User = require("../models/UserModel");
+
+const createUser = (newUser) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, email, password, confirmPassword, phone } = newUser;
+
         try {
-            resolve({})
+            const checkUser = await User.findOne({
+                email: email, // tìm user có email đã tông tại hay chưa
+            });
+            if (checkUser !== null) {
+                resolve({
+                    status: "OK",
+                    message: "Email đã tồn tại!",
+                });
+            }
+
+            const createdUser = await User.create({
+                name,
+                email,
+                password,
+                confirmPassword,
+                phone,
+            });
+            if (createdUser) {
+                resolve({
+                    status: "OK",
+                    message: "Success",
+                    data: createdUser,
+                });
+            }
         } catch (e) {
-            reject(e)
+            reject(e);
         }
-    })
-}
+    });
+};
 
 module.exports = {
-    createUser
-}
+    createUser,
+};
